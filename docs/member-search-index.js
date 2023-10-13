2fc2af6cdecc7d02a8b66ddac1f1f5a1272f1d32
adolforc47 <111707538+adolforc47@users.noreply.github.com>
2023-10-12 23:57:01 -0600

    Adición de javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"poop2_1","c":"POOP2_1","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"poop2_1","c":"POOP2_1","l":"POOP2_1()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poop2_1"}];updateSearchResults();
